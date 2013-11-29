b61612ce2d75956051e5ccd9710b90a3a300a0ba
Luke Bacon <luke@equivalentideas.com>
2013-11-29 12:33:56 +1100

    removed smooth scroll

--- js/functions.js
+++ js/functions.js
@@ -1,27 +1,6 @@
-// Remap jQuery to $
-(function($){
-
-    $(document).ready(function() {
-
-        // Smooth scrolling
-        $('.scroll').click(function(event){
-            event.preventDefault();
-            var thisHash = $(this).attr('href');
-            $('html, body').animate({scrollTop:$(this.hash).offset().top+1}, 300);
-            window.history.replaceState('Object', 'Title', '/'+thisHash);
-        });
-
-
-
     $(document).ready(function() {
     //all heading 1s
-        $('.journal-entry h1').widowFix();
-    //or specific h1s
-        $('#mainColumn h1').widowFix();
+        $('.journal-entry h1 a').widowFix();
 
     });
-
-
-    });
-});
 
