9c2b07059c5ab5deed2d98eb873c5fb2f5312f32
Luke Bacon <luke@equivalentideas.com>
2013-12-12 22:47:38 +1100

    rearranged js

---
+++ js/global.js
@@ -0,0 +1,10 @@
+// WidowFix
+$(document).ready(function() {
+    $('.journal-entry p, .journal-entry h1, .site-intro, .p-name').widowFix();
+
+});
+
+// FitVids
+$(document).ready(function(){
+	$("figure.media").fitVids();
+});
