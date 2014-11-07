884e0af7a909503bf91bfd167bf412790525b459
Luke Bacon <luke@equivalentideas.com>
2014-11-08 10:45:02 +1100

    fix indentation (white space only)

--- js/global.js
+++ js/global.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    $('.p-name, .journal-entry h1').widowFix({
-        letterLimit: 10,
-        prevLimit: 8
-    });
+  $('.p-name, .journal-entry h1').widowFix({
+      letterLimit: 10,
+      prevLimit: 8
+  });
 
-    // WidowFix
-    $('.journal-entry p, .site-intro, .p-summary').widowFix();
+  // WidowFix
+  $('.journal-entry p, .site-intro, .p-summary').widowFix();
 
     // FitVids
 	$(".media-figure").fitVids();
