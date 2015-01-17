4e6e82ea9f0fe38fb889f5d1f8ec1208a915ffeb
Luke Bacon <luke@equivalentideas.com>
2015-01-17 19:21:29 +1100

    refactor js

--- js/build/production.js
+++ js/build/production.js
@@ -1997,8 +1997,7 @@ $(document).ready(function() {
 	$(".media-figure").fitVids();
 
   if ($("#mp-list").length ) {
-    $("#mp-list").prepend("<input id='list-search' class='search list-search-input' placeholder='e.g. Baird or Manly' />");
-    $(".list-search-input").before("<label for='list-search' class='list-search-label'>Filter list by name or electorate</label>");
+    $("#mp-list").prepend("<form><label for='list-search' class='list-search-label'>Filter list by name or electorate</label><input id='list-search' class='search list-search-input' placeholder='e.g. Baird or Manly' /></form>");
 
     var options = {
       valueNames: [ 'p-name', 'p-role' ]
